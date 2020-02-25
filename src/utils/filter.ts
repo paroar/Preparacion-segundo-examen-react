c91a07913ae5a20354d5825150c33392d72f6a33
paroar <prodriguezarmida@gmail.com>
2020-02-25 08:30:31 +0100

    fix rebase

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -1,4 +1,4 @@
-const allKeysFilter = <T extends { [index: string]: any; }>(
+const allKeysFilter = <T extends { [index: string]: any }>(
   xss: T[],
   filter: { [k in keyof T]: (x: T[k]) => boolean }
 ) => {
@@ -6,11 +6,9 @@ const allKeysFilter = <T extends { [index: string]: any; }>(
     Object.entries(filter).every(([k, v]) => {
       return k in xs && Array.isArray(v)
         ? (!("0" in v) || v[0] <= xs[k]) && (!("1" in v) || xs[k] <= v[1])
-        : v === xs[k]
-    }
-    )
+        : v === xs[k] || xs.name.includes(v);
+    })
   );
-}
-
+};
 
 export default allKeysFilter;
